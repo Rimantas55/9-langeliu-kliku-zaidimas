aab79e4a14549e19df99a45d462d65b509cf61d2
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2018-10-14 15:10:26 +0300

    pasizymi tik vienas langelis
    Nepavyko daugiau padaryti, bloga logika arba sintakse

---
+++ 9langeliai.js
@@ -0,0 +1,20 @@
+console.log('working');
+
+var next_btn = document.getElementById('btn1');
+var random_btn = document.getElementById('btn2');
+var block_btn = document.getElementsByClassName('block');
+
+function next_red(){
+	var x = document.getElementsByClassName("block");
+	x[0].style.background = 'red';
+	
+
+}
+
+
+
+
+
+
+
+
