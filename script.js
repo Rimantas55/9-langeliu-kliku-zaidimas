df07d436ee298b0db02c767bf5f432175689d027
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2020-12-04 00:07:52 +0200

    fixed next_red() function

--- script.js
+++ script.js
@@ -1,45 +1,28 @@
 // console.log("working"); // check connetion
 //TODO GENERATE HTML VIA LOOP
 
-// const next_btn = document.getElementById("btn1");
-// const random_btn = document.getElementById("btn2");
 const block_btn = document.getElementsByClassName("block");
+var index = 1;
 
 //* SELECTS RANDOM NUMBER FROM 1-9
 function next_random() {
     clearCell();
-    let random_index = Math.ceil(Math.random() * 9);
-    console.log(random_index);
-    document.getElementById("nr_" + random_index).style.background = "red";
-    Math.floor(Math.random() * 9);
+    index = Math.ceil(Math.random() * 9);
+    document.getElementById("nr_" + index).style.background = "red";
 }
 
+//* CLEARS CELL BY INDEX
 function clearCell() {
-    for (let index = 1; index <= 9; index++) {
-        document.getElementById("nr_" + index).style.background = "grey";
-    }
+    document.getElementById("nr_" + index).style.background = "grey";
 }
 
-//! DOES NOT START FROM RANDOM NUMBER
-//! AFTER RANDOM DOES NOT WORK NEXT BTN
-//! DOES NOT START FROM 1 AFTER HITTING
-//TODO IF NUMBER NEXT REACHES 9 START FROM 1 AGAIN
-let index = 0;
 function next_red() {
-    if (index <= 9) {
+    clearCell();
+    if (index < 9) {
         index++;
         document.getElementById("nr_" + index).style.background = "red";
-        clearOneCell();
-    } else if (index === 10 && index >= 10) {
-        index = 0;
-        clearOneCell();
+    } else {
+        index = 1;
+        document.getElementById("nr_" + index).style.background = "red";
     }
 }
-
-let indexGrey = -1;
-function clearOneCell() {
-    indexGrey++;
-    document.getElementById("nr_" + indexGrey).style.background = "grey";
-}
-
-// elevator saga yra toks game
